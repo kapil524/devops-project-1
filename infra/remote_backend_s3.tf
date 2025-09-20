6ece751874bb7f1198c5a2aaf3c918127ece42a6
“Kapil <kapil.manwani@bt.com>
2025-09-20 18:22:01 +0530

    fix- bucket  region

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket-12345678910"
     key    = "devops-project-1/terraform.tfstate"
-    region = "eu-central-1"
+    region = "eu-west-1"
   }
 }
